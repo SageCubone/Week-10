43ef71967b17aa308d19cce2bf426a9ce8d540f6
jorge <jorgealejandrovaca@gmail.com>
2020-11-23 22:40:47 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+'use strict'
+
+// Easy
+
+// let nums = [99, 77, 85, 45, 100];
+
+// function outputAverage (){
+//     let total = 0;
+//     for(var i = 0; i < nums.length; i++) {
+//     total += nums[i];
+//     }
+//     let avg = total / nums.length;
+//     return avg
+// }
+
+// console.log(outputAverage())
+
+// Medium
+
+let nums2 = [4, 5, 6, 7, 0, 1, 2, 3];
+
+function findTargetValue (target){
+    for(let i = 0; i < nums2.length; i++){
+        let nums2Index = nums2.indexOf(nums2[i]);
+        if(nums2Index === target) {
+            return nums2.length;
+         }
+
+    }
+
+}     
+
+console.log(findTargetValue(5));
